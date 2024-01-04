12ebfe6c75c54ac80dbc786a489ce23685c00303
Kodi <144355274+enbysnack@users.noreply.github.com>
2024-01-04 08:12:06 -0500

    Created files

---
+++ Student Files/PizzaOrder (1).py
@@ -0,0 +1,19 @@
+import random
+import math
+
+class Pizzas:   
+   
+            
+   def getValue(self): 
+      pass
+
+   
+   def toString(self):
+      pass
+
+
+########################
+########################
+#DRIVER SECTION
+      #Enter your code below
+      
